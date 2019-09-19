0ad615789dd3737cdfc883f17f8ce3099d46bd0f
Owen Davies <odavies@gmail.com>
2019-09-19 17:54:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,38 @@
 apt-get update; apt-get install -y vim tmux xinit git build-essential wget curl sudo
 
 apt-get install -y libx11-dev libxft-dev libxinerama-dev
+
+update-alternatives --set editor /usr/bin/vim.basic
+
+sudo apt-get install libxrandr-dev
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+sudo apt-get install libasound2
+
+echo "deb http://ftp.us.debian.org/debian/ unstable main contrib non-free" >> /etc/apt/sources.list
+
+echo "Package: *" >> /etc/apt/preferences
+echo "Pin: release a=stable" >> /etc/apt/preferences
+echo "Pin-Priority: 900" >> /etc/apt/preferences
+echo "" >> /etc/apt/preferences
+echo "Package: *" >> /etc/apt/preferences
+echo "Pin: release a=unstable" >> /etc/apt/preferences
+echo "Pin-Priority: 10" >> /etc/apt/preferences
+
+apt-get update
+apt-get install -t unstable firefox
+
+sudo apt-get install pass -y
+
+sudo apt-get install offlineimap msmtp
+
+echo 'deb https://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
+sudo apt update
+sudo apt install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice
