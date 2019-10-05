1fb1bcf5df053c867ded9c226798b044ad2f7e1a
Owen Davies <odavies@gmail.com>
2019-10-05 09:17:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,10 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > package
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo apt-get update
 sudo apt-get install 
-sudo apt-get install libasound2
+sudo apt-get install libasound2 -y
 
 
 # Install firefox
