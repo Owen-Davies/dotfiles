e88fbf50af55dd62701cb8e81d1c808a6c05a38d
Owen Davies <odavies@gmail.com>
2019-10-05 08:47:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ update-alternatives --set editor /usr/bin/vim.basic
 
 sudo apt-get install libxrandr-dev
 
+# Install Visual Studio Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install code # or code-insiders
+sudo apt-get install 
 sudo apt-get install libasound2
 
+
+# Install firefox
 echo "deb http://ftp.us.debian.org/debian/ unstable main contrib non-free" >> /etc/apt/sources.list
 
 echo "Package: *" >> /etc/apt/preferences
@@ -26,15 +28,14 @@ echo "Package: *" >> /etc/apt/preferences
 echo "Pin: release a=unstable" >> /etc/apt/preferences
 echo "Pin-Priority: 10" >> /etc/apt/preferences
 
-# Install firefox
 apt-get update
 apt-get install -t unstable firefox
 
 # Install pass for password management
 sudo apt-get install pass -y
 
-# Install email (offlineimap, msmtp, neomutt)
-sudo apt-get install offlineimap msmtp neomutt
+# Install email (neomutt)
+sudo apt-get install neomutt isync msmtp
 
 # Install remmina for RDP management
 echo 'deb https://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
@@ -52,5 +53,7 @@ apt-key adv --recv-key --keyserver keyserver.ubuntu.com AD3DD469
 
 sudo apt-get update
 sudo apt install nextcloud-client -y
+
+# Install
 
 
